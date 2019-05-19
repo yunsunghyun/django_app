c362754bab179f69333665b9200e692d74f05df7
ysh151 <ysh151@naver.com>
2019-05-19 21:59:53 +0900

    commit 0519

--- sns/admin.py
+++ sns/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from sns.models import Message, Friend, Group, Good
+
+admin.site.register(Message)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(Good)
